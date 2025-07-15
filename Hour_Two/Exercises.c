2d457f78be74ec2470c2fe64fb046dab13c74ba9
sonny bell <sonnybell2004@gmail.com>
2025-07-15 16:28:58 +0100

    Hour two complete.

---
+++ Hour_Two/Exercises.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// Newline exercise
+int main()
+{
+    printf("It's fun to write my own program in C.\n");
+
+    printf("Hello World!, This is my first C program.\n");
+    return 0;
+}
---
+++ Hour_Two/Find_The_Error.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+// <stdui.h> should be first as print is used first before exit :)
+#include <stdio.h>
+
+// Void shouold be removed
+void main()
+{
+    printf("Howdy, neighbor! This is my first C program.\n");
+    // Exit should be return 0;
+    exit(0);
+}
