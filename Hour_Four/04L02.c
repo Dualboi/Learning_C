c7a3a0073e2143f19c4e5cfaa2b2f6f9193ba035
sonny bell <sonnybell2004@gmail.com>
2025-08-06 17:32:54 +0100

    Hour Four commit, still need to complete workshop.

---
+++ Hour_Four/04L02.c
@@ -0,0 +1,14 @@
+/* 04L02.c: Converting numeric values back to aracters */
+#include <stdio.h>
+
+int main(void)
+{
+    char c1;
+    char c2;
+
+    c1 = 65;
+    c2 = 97;
+    printf("The charactr that has the numeric value of 65 is: %c.\n", c1);
+    printf("The character that has the numeric value of 97 is: %c.\n", c2);
+    return 0;
+}
